8227daf6e56c984ac733d44174b81870db00c355
Eddy Shure <mono@tucana.uberspace.de>
2014-03-02 09:50:37 +0100

    Bare minimum and working title.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ randioactive
 ============
 
 Counting clicks and generating true random numbers.
+
+How does it work?
+-----------------
+
+VOODOO!
+
+"CAN I HAZ TOO?"
+----------------
+
+Of course.
---
+++ randioactive.rb
@@ -0,0 +1,16 @@
+require 'serialport'
+require 'json'
+
+port_str = "/dev/ttyACM0"
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+loop do
+  p sp.gets
+end
+
+sp.close
